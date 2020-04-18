e690e69ca240558d16f1507597e705f59f3b1488
Vlad <kulus20@gmail.com>
2020-04-17 19:42:45 -0500

    started working with slider

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        // adaptiveHeight: true,
+        fade: true,
+        cssEase: 'linear'
+      });
+  });
