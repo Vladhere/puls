46e56daf2e8a390b99ac86c029cf13cc544a2444
Vlad <kulus20@gmail.com>
2020-04-20 22:54:05 -0500

    slider adaptivity 2th

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,22 @@
 
 $(document).ready(function(){
     $('.carousel__inner').slick({
-        dots: true,
         infinite: true,
         speed: 300,
         slidesToShow: 1,
         // adaptiveHeight: true,
         fade: true,
-        cssEase: 'linear'
+        cssEase: 'linear',
+        prevArrow: '<button type="button" class="slick-prev"><i class="left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="right"></button>',
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                arrows: false,
+                dots: true
+              }
+            }
+          ]
       });
   });
